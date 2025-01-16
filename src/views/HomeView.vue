cab3d6b6a5a9bfa31353e5fda2cbd1f3adce19dc
Karlo Stjepanović <karlo.stjepanovic@bytetive.com>
2025-01-16 03:27:46 +0100

    Tweaks

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -18,8 +18,13 @@ import SectionHolderComponent from "@/components/SectionHolderComponent.vue";
     <SectionHolderComponent theme="dark" color-class="status-purple">
         <p v-for="i in 16">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Enim, est illum impedit nihil optio quibusdam ratione repudiandae sed similique veritatis! Assumenda distinctio eos eveniet excepturi laborum, optio perspiciatis quisquam vel.</p>
     </SectionHolderComponent>
+    <SectionHolderComponent color-class="status-blue">
+        <p v-for="i in 8">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Enim, est illum impedit nihil optio quibusdam ratione repudiandae sed similique veritatis! Assumenda distinctio eos eveniet excepturi laborum, optio perspiciatis quisquam vel.</p>
+    </SectionHolderComponent>
 </template>
 
 <style scoped>
-
+p {
+    font-family: 'Tisa Sans', sans-serif;
+}
 </style>
